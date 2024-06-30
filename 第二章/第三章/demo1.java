efc44416379888e91d7d729102cf0a6b7a428ae5
zhuchenyu2008 <zhuchenyu2008@foxmail.com>
2024-06-30 11:45:39 +0800

    1

--- 第二章/第三章/demo1.java
+++ 第二章/第三章/demo1.java
@@ -1,8 +1,9 @@
 public class demo1{
     public static void main(String[] args) {
-        int i,k;
+        int i,k,j;
             i = 10;
             k = 20;
-        System.out.println(i<k);
+            j = 30;
+        System.out.println(i + k + j);
     }
 }
---
+++ 第二章/第三章/demo2.java
@@ -0,0 +1,9 @@
+public class demo2{
+    public static void main(String[] args) {
+        int i = 10 , k = 20 , t = 30;
+        boolean r = (i<k) && (i==t);
+        boolean a = !(i<k);
+        System.out.println(r + "  " + a);
+        
+    }
+}
