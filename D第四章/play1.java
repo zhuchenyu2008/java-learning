00616b485430fa56089a71f61397a47b01ef2801
zhuchenyu2008 <zhuchenyu2008@foxmail.com>
2024-06-30 18:25:13 +0800

    1

--- D第四章/play1.java
+++ D第四章/play1.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 public class play1{
     public static void main(String[] args) {
     Random a = new Random();
-    int b = a.nextInt(101);  //随机生成一个0-100的数字
+    int b = a.nextInt(1);  //随机生成一个0-100的数字
 
     System.out.println("这是一个猜数字小游戏");
     System.out.println("数字的取值范围在 0-100 之间");
     System.out.println("现在请输入你猜测的数字:");
+    
     Scanner input = new Scanner(System.in);
     int c = input.nextInt(); //将输入的数字赋值给c
 
-    int i = c<b?1:2;  //比较b与c的大小
-    
-    switch (i) {
-        case 1:
-            System.out.println("小了");
-            break;
-        case 2:
-            System.out.println("大了");
-            break;
+    if (c>b) {
+        System.out.println("大了");
+    if (c<b) {
+        System.out.println("小了");
             
-        default:
-            System.out.println("恭喜你,猜对了");
-            break;
+        }
+    } else {
+        System.out.println("恭喜你猜对了");
     }
+
+    //判断大小或相等
     
+    System.out.println("正确答案是:" + b);
+
     }
 }
